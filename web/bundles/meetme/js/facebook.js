04baa5bc6d7f5b5bb7d8c64d13a882406c53666f
MENOSCAL <alfonso_menos@hotmail.com>
2016-01-15 21:32:04 -0500

    login facebook

--- web/bundles/meetme/js/facebook.js
+++ web/bundles/meetme/js/facebook.js
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-$(function() {
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
@@ -64,14 +63,7 @@ $(function() {
 
   };
 
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+ 
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
@@ -85,6 +77,6 @@ $(function() {
   }
 
 
-})
+
 
 
